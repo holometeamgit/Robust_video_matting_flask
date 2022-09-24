d14895cc1b58288a9c3874f789842e02cd5ecccb
Ubuntu <ubuntu@ip-172-31-3-76.eu-west-2.compute.internal>
2022-09-24 14:32:38 +0000

    fix non-audio stream issue

--- video_utils.py
+++ video_utils.py
@@ -1,6 +1,13 @@
 import ffmpeg
 import os
 
+def has_audio_streams(file_path):
+    streams = ffmpeg.probe(file_path)["streams"]
+    for stream in streams:
+        if stream["codec_type"] == "audio":
+            return True
+    return False
+
 def video_correction(video_source, audio_source, rotation, av_result):
     """
     Add audio to video and fix orientation if it was recorded in metadata
@@ -12,6 +19,10 @@ def video_correction(video_source, audio_source, rotation, av_result):
     """
 
     stream_in = ffmpeg.input(video_source)
+
+    is_audio_streams_exist = has_audio_streams(video_source)
+    print("is_audio_streams_exist:" + str(is_audio_streams_exist))
+
     audio_in = stream_in.audio
 
     stream_out = ffmpeg.input(audio_source)
@@ -25,5 +36,10 @@ def video_correction(video_source, audio_source, rotation, av_result):
         # if rotation == 180:  # there were no examples of such videos yet
         #     video_out = stream_out.video.filter("transpose", 2).filter("transpose", 2)
 
-    out = ffmpeg.output(video_out, audio_in, av_result, **{'q:v': 0})
+    out = None
+    if is_audio_streams_exist is True:
+        out = ffmpeg.output(video_out, audio_in, av_result, **{'q:v': 0})
+    else:
+        out = ffmpeg.output(video_out, av_result, **{'q:v': 0})
+
     out.global_args('-y').run()
